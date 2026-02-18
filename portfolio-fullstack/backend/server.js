4f956b55b17ce22ff770d16f9df9b5e5f73aa9ac
vansh chauhan <vanshrc7@gmail.com>
2026-02-19 02:40:52 +0530

    update

--- portfolio-fullstack/backend/server.js
+++ portfolio-fullstack/backend/server.js
@@ -58,8 +58,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use('/api/', limiter);
 
 // Serve uploaded files
