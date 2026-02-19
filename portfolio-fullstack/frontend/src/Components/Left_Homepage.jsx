3575e8435f1833c563bb2bd331b9fec2f0f5cf00
vansh chauhan <vanshrc7@gmail.com>
2026-02-19 09:14:20 +0530

    update

--- portfolio-fullstack/frontend/src/Components/Left_Homepage.jsx
+++ portfolio-fullstack/frontend/src/Components/Left_Homepage.jsx
@@ -46,7 +46,7 @@ const Right_Homepage = () => {
                  "
                 >
 
-                 Vansh 
+                 Vansh Chauhan.
 
                 </span>
                 </p>
@@ -54,7 +54,7 @@ const Right_Homepage = () => {
 
           <p className="text-green-300 text-2xl mt-3">
 
-            &gt;Backend Engine
+            &gt;Backend Engineer
             
           </p>
 
